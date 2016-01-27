dd8b1fcfc05c88f3acc6ef723ef19db5785b9035
Nitesh Shaw <nitesh_shaw@hotmail.com>
2016-01-27 12:48:53 +0530

    Port changed to variable

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var app = express();
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });
-
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+var port = Number(process.env.PORT || 3000)
+app.listen(port, function () {
+  console.log('Example app listening on port '+port);
 });
